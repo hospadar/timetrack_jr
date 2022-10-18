12e61faf64b8234ac9de8c241c18b94f02bb52cd
Luke Hospadaruk <luke@hospadaruk.org>
2022-10-18 13:15:15 -0400

    more stuff

--- src/cli.rs
+++ src/cli.rs
@@ -14,8 +14,71 @@ pub struct Cli {
 
 #[derive(Subcommand, Debug)]
 pub enum Commands {
-    /// Set up DB and configure options
-    Config,
-    /// Listen for key events to trigger time tracking
-    Listen,
+    ///Set up DB and configure options
+    Config {
+        #[command(subcommand)]
+        config_command: ConfigCommands
+    },
+    ///Start or stop timing an activity, or amend an existing time
+    Log {
+        #[command(subcommand)]
+        log_command: LogCommands
+    },
+    ///Globally listen for key events to trigger time tracking
+    Listen {
+        #[arg(long, short)]
+        ical_file:Option<String>,
+        #[arg(long, short)]
+        csv_file:Option<String>, 
+        #[arg(long, short)]
+        json_file:Option<String>,
+        #[arg(long, default_value = "-1")]
+        days_to_export:i64
+    },
 }
+
+
+#[derive(Subcommand, Debug)]
+pub enum ConfigCommands {
+    ///Show config options and currently-registered-categories
+    Show,
+    ///Create a new category that you can use for time tracking
+    AddCategory {
+        category_name: String,
+        #[arg(short,long)]
+        set_key_sequence: bool
+    }, 
+    ///Delete a category
+    DeleteCategory {
+        category_name: String,
+        #[arg(short,long)]
+        delete_logged_times: bool
+    },
+    ///Set the key sequence to trigger the start of timing when using listen mode
+    SetKeySequence {
+        category_name: String
+    },
+    ///Set a global option
+    SetOption{
+        option_name: String,
+        option_value: String,
+    },
+}
+
+#[derive(Subcommand, Debug)]
+pub enum LogCommands {
+    StartTiming {
+        category_name: String
+    },
+    StopTiming,
+    AmendTime {
+        time_id: u64,
+        #[arg(short, long)]
+        start_time: String,
+        #[arg(short, long)]
+        end_time: String,
+        #[arg(short, long)]
+        category: String
+    }
+}
+
--- src/db.rs
+++ src/db.rs
@@ -1,6 +1,20 @@
 use rusqlite::{Connection};
+use std::collections::{BTreeMap};
 
 const VERSION: &str = env!("CARGO_PKG_VERSION");
+const PRETTY_INDENT: u16 = 2;
+
+pub struct Category {
+    name: String,
+    keys: Option<String>
+}
+
+pub struct TimeWindow {
+    id: u64,
+    category: String,
+    start_time: u64,
+    end_time: Option<u64>,
+}
 
 pub fn initialize_db(conn: &Connection) -> Result<&Connection, rusqlite::Error> {
 
@@ -24,8 +38,7 @@ pub fn initialize_db(conn: &Connection) -> Result<&Connection, rusqlite::Error> 
     conn.execute(
         "CREATE TABLE IF NOT EXISTS categories (
             name TEXT PRIMARY KEY,
-            keys TEXT NOT NULL,
-            press_and_hold BOOLEAN NOT NULL
+            keys TEXT NOT NULL
         )", ()
     )?;
 
@@ -33,8 +46,8 @@ pub fn initialize_db(conn: &Connection) -> Result<&Connection, rusqlite::Error> 
         "CREATE TABLE IF NOT EXISTS times (
             id INTEGER PRIMARY KEY,
             category TEXT NOT NULL,
-            start_time INTEGER NOT NULL,
-            end_time INTEGER NOT NULL CHECK (end_time >= start_time),
+            start_time INTEGER NOT NULL CHECK (start_time > 0),
+            end_time INTEGER CHECK (end_time is null or end_time >= start_time),
             FOREIGN KEY(category) REFERENCES categories(name)
         )", ()
     )?;
@@ -42,4 +55,30 @@ pub fn initialize_db(conn: &Connection) -> Result<&Connection, rusqlite::Error> 
     conn.execute("COMMIT", ())?;
 
     return Result::Ok(&conn);
+}
+
+pub fn print_config(conn: &Connection) -> Result<(), rusqlite::Error> {
+    let mut options: BTreeMap<String, String> = BTreeMap::new();
+    let mut stmt = conn.prepare("SELECT name, value FROM options")?;
+    let mut rows = stmt.query(())?;
+
+    while let Some(row) = rows.next()? {
+        options.insert(row.get(0)?, row.get(1)?);
+    }
+
+    let mut categories: Vec<String> = Vec::new();
+    let mut stmt = conn.prepare("SELECT name FROM categories order by name")?;
+    let mut rows = stmt.query(())?;
+
+    while let Some(row) = rows.next()? {
+        categories.push(row.get(0)?);
+    }
+
+    println!(
+        "Options:\n{}\n==============\nCategories:\n{}", 
+        json::stringify_pretty(options, PRETTY_INDENT),
+        json::stringify_pretty(categories, PRETTY_INDENT)
+    );
+
+    return Ok(());
 }
