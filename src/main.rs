27789e93fcb2877d4c92c9a5e018b8dcf1af6c48
Luke Hospadaruk <luke@hospadaruk.org>
2022-11-11 23:28:10 -0500

    copyright notice

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+/*
+Copyright 2022 Luke Hospadaruk
+This file is part of Timetrack Jr.
+Timetrack Jr. is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+Timetrack Jr. is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+You should have received a copy of the GNU General Public License along with Timetrack Jr. If not, see <https://www.gnu.org/licenses/>. 
+*/
 use clap::Parser;
 use std::{
     num::ParseIntError,
